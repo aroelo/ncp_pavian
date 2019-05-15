1851a997ee27feab6476f6d67367b332478007f9
shiny account RAVBIO01 <shiny@naktuinbouw.net>
2019-05-15 11:56:03 +0200

    local copy of app.R to remote repo

--- inst/shinyapp/app.R
+++ inst/shinyapp/app.R
@@ -1,9 +1,14 @@
+library(reticulate)
 library(shiny)
+use_condaenv("pavian")
+
+
 
 if(!require(pavian)){
   options(repos = c(CRAN = "http://cran.rstudio.com"))
-  if (!require(remotes)) { install.packages("remotes") }
-  remotes::install_github("fbreitwieser/pavian")
+  #runUrl("http://gitlab.naktuinbouw.net/bioinformatics/pavian/blob/master/pavian_0.8.4.tar.gz",filetype=".tar.gz.")
+  #download.file("http://gitlab.naktuinbouw.net/bioinformatics/pavian/raw/master/pavian_0.8.4.tar.gz", "/tmp/pavian.tar.gz")
+  #install.packages("/tmp/pavian.tar.gz", repos = NULL, type="source")
   library(pavian)
 }
 
@@ -22,7 +27,7 @@ if (!dir.exists(rappdirs::user_config_dir("pavian", expand = FALSE))) {
 ## Shiny options
 # Specify the maximum web request size, which serves as a size limit for file uploads. If unset, the maximum request size defaults to 5MB
 # see https://shiny.rstudio.com/reference/shiny/latest/shiny-options.html for global shiny options
-# options(shiny.maxRequestSize = 50 * 1024 ^ 2) # set to 50 MB
+options(shiny.maxRequestSize = 50 * 1024 ^ 2) # set to 50 MB
 ## do not set shiny.maxRequestSize here, because it overrides user options!
 
 ## DT options
