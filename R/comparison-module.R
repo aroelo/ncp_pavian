93ca0b5a253c546df5da0cfbbc7b617249ea0eea
Shiny User <shiny@rav-bio01>
2019-12-10 02:18:13 +0100

    Use complete path of pavian file instead of name.

--- R/comparison-module.R
+++ R/comparison-module.R
@@ -488,6 +488,14 @@ comparisonModule <- function(input, output, session, sample_data, tax_data, clad
     for (i in 1:nrow(sample_data())) {
       sample_files[i] = sample_data()[["ReportFile"]][i]
     }
+    browser()
+    js_dict <-NULL
+    for (i in seq(1, nrow(sample_data()))) {
+      key_value<- paste0(sample_data()$Name[i], "\":\"", sample_data()$ReportFilePath[i])
+      js_dict <- paste0(js_dict, key_value, sep = "\",\n\"")
+    }
+    js_dict <- paste0("\"",substr(js_dict, 0, nchar(js_dict)-3))
+    # session$sendCustomMessage(type="pavian_files", message=sample_data()$ReportFilePath)
     #filtered_clade_reads()
     DT::datatable(myDf,
                   container=myContainer,
@@ -513,7 +521,8 @@ comparisonModule <- function(input, output, session, sample_data, tax_data, clad
                       search = isolate(dt_options$search)
                     )
                   ),
-                  callback = htmlwidgets::JS('
+                  callback = htmlwidgets::JS(paste('
+                     var pavian_files_dict = {', js_dict, '};
                      table.on("dblclick.dt","tr", function() {
                      var data=table.row(this).data();
                      var data1=data[0].replace(/.*>/,"")
@@ -529,8 +538,10 @@ comparisonModule <- function(input, output, session, sample_data, tax_data, clad
                      var taxid_string = table.cell(rowIdx,2).data();
                      var cell = table.cell(this);
                      
+                     var pavian_file = pavian_files_dict[colName];
                      //var link_str = \'<p><a href="http://***REMOVED***/cgi-bin/download_pavian_data.py?taxid=\' + taxid_string + \'&amp;sample=\' + colName + \'&amp;action=download" target="_blank">Download data</a></p><p><a href="http://***REMOVED***/cgi-bin/download_pavian_data.py?taxid=\' + taxid_string + \'&amp;sample=\' + colName + \'&amp;action=viewreads" target="_blank">View reads and blast</a></p><p><a href="http://***REMOVED***/cgi-bin/download_pavian_data.py?taxid=\' + taxid_string + \'&amp;sample=\' + colName + \'&amp;action=jbrowse" target="_blank">Visualise in Jbrowse</a></p>\';
-                     var link_str = \'<p><a href="http://***REMOVED***:***REMOVED***/download_pavian_data?taxid=\' + taxid_string + \'&amp;sample=\' + colName + \'&amp;action=download" target="_blank">Download data</a></p><p><a href="http://***REMOVED***:***REMOVED***/download_pavian_data?taxid=\' + taxid_string + \'&amp;sample=\' + colName + \'&amp;action=viewreads" target="_blank">View reads and blast</a></p><p><a href="http://***REMOVED***:***REMOVED***/download_pavian_data?taxid=\' + taxid_string + \'&amp;sample=\' + colName + \'&amp;action=jbrowse" target="_blank">Visualise in Jbrowse</a></p>\';
+                     //var link_str = \'<p><a href="http://***REMOVED***:***REMOVED***/download_pavian_data?taxid=\' + taxid_string + \'&amp;sample=\' + colName + \'&amp;action=download" target="_blank">Download data</a></p><p><a href="http://***REMOVED***:***REMOVED***/download_pavian_data?taxid=\' + taxid_string + \'&amp;sample=\' + colName + \'&amp;action=viewreads" target="_blank">View reads and blast</a></p><p><a href="http://***REMOVED***:***REMOVED***/download_pavian_data?taxid=\' + taxid_string + \'&amp;sample=\' + colName + \'&amp;action=jbrowse" target="_blank">Visualise in Jbrowse</a></p>\';
+                     var link_str = \'<p><a href="http://***REMOVED***:***REMOVED***/download_pavian_data?taxid=\' + taxid_string + \'&amp;sample=\' + pavian_file + \'&amp;action=download" target="_blank">Download data</a></p><p><a href="http://***REMOVED***:***REMOVED***/download_pavian_data?taxid=\' + taxid_string + \'&amp;sample=\' + pavian_file + \'&amp;action=viewreads" target="_blank">View reads and blast</a></p><p><a href="http://***REMOVED***:***REMOVED***/download_pavian_data?taxid=\' + taxid_string + \'&amp;sample=\' + pavian_file + \'&amp;action=jbrowse" target="_blank">Visualise in Jbrowse</a></p>\';
                     if (colName != "Name" && colName != "Rank" && colName != "TID"&& colName != "Max" && colName != "Lineage"){
                     $(cell).qtip({
                       overwrite: false,
@@ -550,7 +561,7 @@ comparisonModule <- function(input, output, session, sample_data, tax_data, clad
                       }
                     }, event); // Note we passed the event as the second argument. Always do this when binding within an event handler!
                     }
-                })')) %>% formatDT(nSamples = nrow(sample_data()),
+                })'))) %>% formatDT(nSamples = nrow(sample_data()),
                                    numericColumns = numericColumns(),
                                    statsColumns = input$opt_statsColumns,
                                    nColumnsBefore = ncol(tax_data()) - 1,
